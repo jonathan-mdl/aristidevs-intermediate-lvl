ab2fb62848a36fb42b0bb7ef87debc7a60bb69bc
Jonathan <jnthnwebapps@gmail.com>
2025-01-28 17:00:14 -0300

    git add .

---
+++ app/src/main/java/com/example/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.horoscapp.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horoscapp.R
+import com.example.horoscapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    /* Inicializa y configura la navegación entre fragmentos */
+    private fun initNavigation() {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment  // Obtiene el fragmento que actúa como anfitrión de navegación (NavHostFragment).
+        navController = navHost.navController // Asigna el controlador de navegación del anfitrión.
+        binding.bottomNavView.setupWithNavController(navController) // Vincula el BottomNavigationView con el controlador de navegación.
+    }
+}
